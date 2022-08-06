93dc38b49b3d137530fc8f110124ee030a9ad43a
wally ramirez <wramirezs4@miumg.edu.gt>
2022-08-05 21:05:23 -1000

    cracion de formulario para clientes

---
+++ src/java/ClienteController.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import clases.ClienteModell;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Wally Ramirez
+ */
+@WebServlet(urlPatterns = {"/NewServlet"})
+public class ClienteController extends HttpServlet {
+ClienteModell clientemodell;
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            clientemodell=new ClienteModell();
+            
+            clientemodell.setCodigo(request.getParameter("codigo"));
+            clientemodell.setNombre(request.getParameter("nombre"));
+            clientemodell.setApellido(request.getParameter("apellido"));
+            clientemodell.setDireccion(request.getParameter("direccion"));
+            clientemodell.setCorreo(request.getParameter("correo"));
+            clientemodell.setTelefono(request.getParameter("telefono"));
+            
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet NewServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1> Gracias Por Registrarse </h1><br>");
+            out.println("<h1>" + clientemodell.getCodigo() +  "</h1>");
+            out.println("<h1>" + clientemodell.getNombre() +  "</h1>");
+            out.println("<h1>" + clientemodell.getApellido() + "</h1>");
+            out.println("<h1>" + clientemodell.getDireccion() +  "</h1>");
+            out.println("<h1>" + clientemodell.getCorreo() +  "</h1>");
+            out.println("<h1>" + clientemodell.getTelefono() +  "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+   
+
+}
---
+++ src/java/clases/ClienteModell.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author Wally Ramirez
+ */
+public class ClienteModell {
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+   private String codigo;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String correo;
+    private String telefono;
+    
+     public ClienteModell(){
+         this.nombre="Alumno de ejemplo";
+     }
+     
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public static class alumno {
+
+        public alumno() {
+        }
+    }
+
+    public static class clientemodell {
+
+        public clientemodell() {
+        }
+    }
+    
+}
